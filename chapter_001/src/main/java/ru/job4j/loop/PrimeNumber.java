9ccd1e6f39a6782636a7ffe2fd48acf5b12d0802
Andrey Varshavsky <nulnoil@outlook.com>
2019-10-05 02:24:39 +0300

    6-5.6. Простые числа.[#185325]

---
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,20 @@
+package ru.job4j.loop;
+
+public class PrimeNumber {
+    public int calc(int finish) {
+        int count = 0;
+
+        for (int i = 2; i <= finish; i++){
+
+            for (int ii = 2; ii <= i; ii++) {
+                if (i % ii == 0 && i != ii) {
+                    break;
+                } else if (i == ii) {
+                    count++;
+                }
+            }
+
+        }
+        return count;
+    }
+}

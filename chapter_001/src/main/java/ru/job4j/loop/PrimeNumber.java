2f95ce58e02930a5c0b37eca35edb4fc2dd7dfc0
Andrey Varshavsky <nulnoil@outlook.com>
2019-10-05 15:12:11 +0300

    6-5.5. Простое число.[#185327] проход true-false, вместо false-true

--- chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -4,17 +4,25 @@ public class PrimeNumber {
     public int calc(int finish) {
         int count = 0;
 
-        for (int i = 2; i <= finish; i++){
-
+        for (int i = 2; i <= finish; i++) {
+            count++;
             for (int ii = 2; ii <= i; ii++) {
                 if (i % ii == 0 && i != ii) {
+                    count--;
                     break;
-                } else if (i == ii) {
-                    count++;
                 }
             }
-
         }
         return count;
     }
 }
+
+//        for (int i = 2; i <= finish; i++){
+//                for (int ii = 2; ii <= i; ii++) {
+//                if (i % ii == 0 && i != ii) {
+//                break;
+//                } else if (i == ii) {
+//                count++;
+//                }
+//                }
+//                }

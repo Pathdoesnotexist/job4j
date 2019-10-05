0435d6c6c2c892fd431535ac1aea41ac84912030
Andrey Varshavsky <nulnoil@outlook.com>
2019-10-06 00:01:18 +0300

    6-5.6. Простые числа.[#185325] Работа через класс CheckPrimeNumber

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -1,7 +1,7 @@
 package ru.job4j.loop;
 
 public class CheckPrimeNumber {
-    public boolean check(int finish) {
+    public static boolean check(int finish) {
         boolean prime = true;
         for (int i = 2; i < finish; i++) {
             if (finish % i == 0) {
--- chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -4,17 +4,29 @@ public class PrimeNumber {
     public int calc(int finish) {
         int count = 0;
         for (int i = 2; i <= finish; i++) {
-            count++;
-            for (int ii = 2; ii <= i; ii++) {
-                if (i % ii == 0 && i != ii) {
-                    count--;
-                    break;
-                }
+            if(CheckPrimeNumber.check(i)) {
+                count++;
             }
+
+
         }
         return count;
     }
 }
+
+//    public int calc(int finish) {
+//        int count = 0;
+//        for (int i = 2; i <= finish; i++) {
+//            count++;
+//            for (int ii = 2; ii <= i; ii++) {
+//                if (i % ii == 0 && i != ii) {
+//                    count--;
+//                    break;
+//                }
+//            }
+//        }
+//        return count;
+//    }
 
 //    public int calc(int finish) {
 //        int count = 0;

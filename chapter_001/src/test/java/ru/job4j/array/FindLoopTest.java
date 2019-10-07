7199cd9bff16da9f4bb48e9ad6b0f9e6fb141025
Andrey Varshavsky <nulnoil@outlook.com>
2019-10-08 01:59:27 +0300

    7-6.5. Поиск индекса в диапазоне.[#185342]

--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -27,7 +27,7 @@ public class FindLoopTest {
     }
 
     @Test
-    public void caseNotFound() {
+    public void caseNotFound1() {
         FindLoop find = new FindLoop();
         int[] input = new int[] {-2, 13, 543, 32, 0, -18, 2, 13, -8};
         int value = 10;
@@ -35,4 +35,40 @@ public class FindLoopTest {
         int expect = -1;
         assertThat(result, is(expect));
     }
+
+    @Test
+    public void caseFoundWithPoints1() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOfWithPoints(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void caseFoundWithPoints2() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {-2, 13, 543, 32, 0, -8, 2, 13, -18};
+        int value = -8;
+        int start = 3;
+        int finish = 6;
+        int result = FindLoop.indexOfWithPoints(input, value, start, finish);
+        int expect = 5;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void caseNotFoundWithPoints1() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {-2, 13, 543, 32, 0, -18, 2, 13, -8};
+        int value = 10;
+        int start = 1;
+        int finish = 7;
+        int result = FindLoop.indexOfWithPoints(input, value, start, finish);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+
 }

52fffa0c9646a9759725b7539d200333413cb38b
Andrey Varshavsky <nulnoil@outlook.com>
2019-10-07 23:33:34 +0300

    7-6.2. Перевернуть массив.[#185340]

---
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+
+public class Turn {
+    public int[] back(int[] array) {
+        double oddEvenLength = Math.floor(array.length/2);
+        for (int i = 0; i < oddEvenLength; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - (i+1)];
+            array[array.length - (i+1)] = temp;
+        }
+        return array;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/TurnTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class TurnTest {
+    @Test
+    public void case1() {
+        Turn turner = new Turn();
+        int[] input = new int[] {4, 1, 6, 2};
+        int[] result = turner.back(input);
+        int[] expect = new int[] {2, 6, 1, 4};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void case2() {
+        Turn turner = new Turn();
+        int[] input = new int[] {1, 2, 3, 4, 5};
+        int[] result = turner.back(input);
+        int[] expect = new int[] {5, 4, 3, 2, 1};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void case3() {
+        Turn turner = new Turn();
+        int[] input = new int[] {4, 1, 6, 2, 4, 1, 6, 2};
+        int[] result = turner.back(input);
+        int[] expect = new int[] {2, 6, 1, 4, 2, 6, 1, 4};
+        assertThat(result, is(expect));
+    }
+}

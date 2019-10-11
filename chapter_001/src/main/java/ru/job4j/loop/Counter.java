d6fa993cb7cc6fbbb9eabb0e26347a8a919270fe
Andrey Varshavsky <nulnoil@outlook.com>
2019-10-11 16:33:06 +0300

    L1-002 0-0.2. Чтение кода и базовые ошибки.[#185423]

--- chapter_001/src/main/java/ru/job4j/loop/Counter.java
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -3,7 +3,6 @@ package ru.job4j.loop;
 public class Counter {
     public static int add(int start, int finish) {
         int sum = 0;
-
         for (int i = start; i <= finish; i++) {
         sum = i % 2 == 0 ? sum + i : sum;
         }

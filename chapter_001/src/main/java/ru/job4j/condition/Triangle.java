7299909df0f14ee1a53bd4f3746617bf1f4abd44
Andrey Varshavsky <nulnoil@outlook.com>
2019-10-17 15:07:32 +0300

    L1-002 1-2.2. Рефакторинг - Площадь треугольника.[#185455]

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -1,7 +1,77 @@
 package ru.job4j.condition;
 
 public class Triangle {
-    public static boolean exist(double ab, double ac, double bc) {
-    return ab + ac > bc && ab + bc > ac && ac + bc > ab;
+    private Point firstPoint;
+    private Point secondPoint;
+    private Point thirdPoint;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.firstPoint = ap;
+        this.secondPoint = bp;
+        this.thirdPoint = cp;
+    }
+
+    /**
+     * Метод проверяет можно ли построить треугольник с заданными координатами вершин.
+     * @return false - если фигура точка или отрезок. true - треугольник
+     */
+    public boolean exist() {
+        double ab =  firstPoint.distance(secondPoint);
+        double bc =  secondPoint.distance(thirdPoint);
+        double ac =  thirdPoint.distance(firstPoint);
+        return ab != 0 && bc != 0 && ac != 0;
+    }
+
+    /**
+     * Метод вычисления периметра по длинам сторон.
+     * @return Периметр или -1 если треугольник не существует
+     */
+    public double period() {
+        if (this.exist()) {
+            double ab = firstPoint.distance(secondPoint);
+            double bc = secondPoint.distance(thirdPoint);
+            double ac = thirdPoint.distance(firstPoint);
+            return ab + bc + ac;
+        } else {
+            return -1;
+        }
+    }
+
+    /**
+     * Метод вычисляет площадь треугольника по формуле.
+     * @return площадь треугольника или -1 если треугольник не существует
+     */
+    public double area() {
+        if (this.exist()) {
+            double ab = firstPoint.distance(secondPoint);
+            double bc = firstPoint.distance(thirdPoint);
+            double ac = secondPoint.distance(thirdPoint);
+            double p = this.period()/2;
+        return Math.sqrt(p * (p - ab) * (p - bc) * (p - ac));
+        } else {
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Point a = new Point(0,2);
+        Point b = new Point(1,4);
+        Point c = new Point(4,0);
+        Triangle exampleOne = new Triangle(a, b, c);
+        System.out.println(exampleOne.exist());
+        System.out.println(exampleOne.area());
+        System.out.println(exampleOne.period());
+
+        Point a2 = new Point(0,0);
+        Point b2 = new Point(0,0);
+        Point c2 = new Point(0,0);
+        Triangle exampleTwo = new Triangle(a2, b2, c2);
+        System.out.println(exampleTwo.exist());
+        System.out.println(exampleTwo.area());
+        System.out.println(exampleTwo.period());
     }
 }
+
+
+
+
--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -1,25 +1,56 @@
 package ru.job4j.condition;
 
+import org.junit.Assert;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class TriangleTest {
     @Test
-    public void whenExist() {
-        boolean result = Triangle.exist(2.0, 2.0, 2.0);
-        assertThat(result, is(true));
+    public void whenTriangle() {
+        Point a = new Point(0,2);
+        Point b = new Point(1,4);
+        Point c = new Point(4,0);
+        Triangle abc = new Triangle(a, b, c);
+        boolean exist = abc.exist();
+        double p = abc.period();
+        double s = abc.area();
+        double pOut = 11.70;
+        double sOut = 5.0;
+        assertThat(exist, is(true));
+        Assert.assertEquals(sOut, s, 0.01);
+        Assert.assertEquals(pOut, p, 0.01);
     }
 
     @Test
-    public void whenExist2() {
-        boolean result = Triangle.exist(5.0, 6.0, 2.0);
-        assertThat(result, is(true));
+    public void whenPoint() {
+        Point a = new Point(0,0);
+        Point b = new Point(0,0);
+        Point c = new Point(0,0);
+        Triangle abc = new Triangle(a, b, c);
+        boolean exist = abc.exist();
+        double p = abc.period();
+        double s = abc.area();
+        double pOut = -1;
+        double sOut = -1;
+        assertThat(exist, is(false));
+        Assert.assertEquals(sOut, s, 0.01);
+        Assert.assertEquals(pOut, p, 0.01);
     }
 
     @Test
-    public void whenNotExist() {
-        boolean result = Triangle.exist(8.0, 2.0, 2.0);
-        assertThat(result, is(false));
+    public void whenLine() {
+        Point a = new Point(0,1);
+        Point b = new Point(3,8);
+        Point c = new Point(0,1);
+        Triangle abc = new Triangle(a, b, c);
+        boolean exist = abc.exist();
+        double p = abc.period();
+        double s = abc.area();
+        double pOut = -1;
+        double sOut = -1;
+        assertThat(exist, is(false));
+        Assert.assertEquals(sOut, s, 0.01);
+        Assert.assertEquals(pOut, p, 0.01);
     }
 }

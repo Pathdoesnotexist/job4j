1845ba5fab7f2eaa58689a9ffd73c61fe2d22e43
Andrey Varshavsky <nulnoil@outlook.com>
2019-10-17 15:43:08 +0300

    L1-002 1-3. Перегрузить метод max для трех чисел[#185457]

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -1,9 +1,15 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int max(int left, int right) {
-        int result = left > right ? left : right;
-        return result;
+
+
+    public static int maximum(int first, int second) {
+        return Math.max(first, second);
     }
+
+    public static int maximum(int first, int second, int three) {
+        return maximum(maximum(first, second), three);
+    }
+
 }
 
--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -6,20 +6,32 @@ import static org.junit.Assert.assertThat;
 
 public class MaxTest {
     @Test
-    public void whenMax1To2Then2() {
-        int result = Max.max(1, 2);
+    public void whenMax2() {
+        int result = Max.maximum(1, 2);
         assertThat(result, is(2));
     }
 
     @Test
-    public void whenMax5To1Then5() {
-        int result = Max.max(5, 1);
+    public void whenMax5() {
+        int result = Max.maximum(5, 1);
         assertThat(result, is(5));
     }
 
     @Test
-    public void whenMax3To3Then3() {
-        int result = Max.max(3, 3);
+    public void whenMax3() {
+        int result = Max.maximum(3, 3);
         assertThat(result, is(3));
+    }
+
+    @Test
+    public void whenMax6() {
+        int result = Max.maximum(3, 6, 1);
+        assertThat(result, is(6));
+    }
+
+    @Test
+    public void whenMax0() {
+        int result = Max.maximum(-6, -1, 0);
+        assertThat(result, is(0));
     }
 }

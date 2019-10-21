e300c19b4af775dcd0710516a06614fd6f1d55d3
Andrey Varshavsky <nulnoil@outlook.com>
2019-10-21 16:33:16 +0300

    L1-002 2-2. Перегрузка конструктора.[#185472]

--- chapter_002/src/main/java/ru/job4j/oop/Animal.java
+++ chapter_002/src/main/java/ru/job4j/oop/Animal.java
@@ -2,12 +2,12 @@ package ru.job4j.oop;
 
 public class Animal {
 
-    public Animal(){
+    public Animal(String name){
         System.out.println(getClass());
     }
 
     public static void main(String[] args) {
-        Animal cat = new Animal();
+        Animal cat = new Animal("Garfield");
     }
 }
 
--- chapter_002/src/main/java/ru/job4j/oop/Predator.java
+++ chapter_002/src/main/java/ru/job4j/oop/Predator.java
@@ -1,4 +1,7 @@
 package ru.job4j.oop;
 
 public class Predator extends Animal {
+    public Predator(String name){
+        super(name);
+    }
 }

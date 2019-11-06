a3c22fde790791836f8ac63b85b7c4ad1c3ea429
Andrey Varshavsky <nulnoil@outlook.com>
2019-11-06 21:49:24 +0300

    L1-002 4-9.1 Рефакторинг теста @ Before @ After[#185427]

--- chapter_002/src/test/java/ru/job4j/stragery/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/stragery/PaintTest.java
@@ -1,12 +1,28 @@
 package ru.job4j.stragery;
 
-import org.junit.Test;
+import org.junit.*;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 public class PaintTest {
+    // поле содержит дефолтный вывод в консоль.
+    private final PrintStream stdout = System.out;
+    // буфер для результата.
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
 
     @Test
     public void whenTriangle() {
@@ -23,30 +39,38 @@ public class PaintTest {
         String expected = "++++++++++\n" + "+        +\n" + "+        +\n" + "++++++++++";
         assertThat(created, is(expected));
     }
-//    @Test
-//    public void whenDrawSquare() {
-//        // получаем ссылку на стандартный вывод в консоль.
-//        PrintStream stdout = System.out;
-//        // Создаем буфер для хранения вывода.
-//        ByteArrayOutputStream out = new ByteArrayOutputStream();
-//        //Заменяем стандартный вывод на вывод в пямять для тестирования.
-//        System.setOut(new PrintStream(out));
-//        // выполняем действия в консоль.
-//        new Paint().draw(new Square());
-//        // проверяем результат вычисления
-//        assertThat(
-//                new String(out.toByteArray()),
-//                is(
-//                        new StringBuilder()
-//                                .append("++++++++++\n")
-//                                .append("+        +\n")
-//                                .append("+        +\n")
-//                                .append("++++++++++")
-//                                .append(System.lineSeparator())
-//                                .toString()
-//                )
-//        );
-//        // возвращаем обратно стандартный вывод в консоль.
-//        System.setOut(stdout);
-//    }
+
+    @Test
+    public void whenDrawSquare() {
+        new Paint().draw(new Square());
+        assertThat(
+                this.out.toString(),
+                is(
+                        new StringBuilder()
+                                .append("++++++++++\n")
+                                .append("+        +\n")
+                                .append("+        +\n")
+                                .append("++++++++++")
+                                .append(System.lineSeparator())
+                                .toString()
+                )
+        );
+    }
+
+    @Test
+    public void whenDrawTriangle() {
+        new Paint().draw(new Triangle());
+        assertThat(
+                this.out.toString(),
+                is(
+                        new StringBuilder()
+                                .append("      +     \n")
+                                .append("    +   +   \n")
+                                .append("  +       + \n")
+                                .append("+++++++++++++")
+                                .append(System.lineSeparator())
+                                .toString()
+                )
+        );
+    }
 }

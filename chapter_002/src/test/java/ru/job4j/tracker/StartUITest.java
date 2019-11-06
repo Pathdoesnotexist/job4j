01d3a2a9d674dfe613a7240bdc7d50e8fdf6c089
Andrey Varshavsky <nulnoil@outlook.com>
2019-11-06 21:24:08 +0300

    L1-002 4-9. Написать тесты на StartUI.[#185431]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -46,4 +46,14 @@ public class StartUITest {
         assertNull(tracker.items[2]);
     }
 
+    @Test
+    public void whenExit() {
+        StubInput input = new StubInput(
+                new String[] {"0"}
+        );
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] { action });
+        assertThat(action.isCall(), is(true));
+    }
+
 }

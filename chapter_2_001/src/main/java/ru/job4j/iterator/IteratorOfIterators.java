8473f6ddc677953c4a5632225f5f613dccad7581
Andrey Varshavsky <nulnoil@outlook.com>
2020-01-04 18:29:19 +0300

    L2-001 1-5.1.4. 5.1.4. Создать convert(Iterator<Iterator>)[#202670] Refactoring002

--- chapter_2_001/src/main/java/ru/job4j/iterator/IteratorOfIterators.java
+++ chapter_2_001/src/main/java/ru/job4j/iterator/IteratorOfIterators.java
@@ -4,60 +4,47 @@ package ru.job4j.iterator;
 import java.util.*;
 
 /**
- * CLASSDESC_CLASSDESC_CLASSDESC_CLASSDESC_CLASSDESC_CLASSDESC_CLASSDESC_CLASSDESC
+ * Итератор итераторов
  * @author Andrey Varshavsky
  * @version 1.0
  * @since 27.12.2019
  */
-public class IteratorOfIterators implements Iterator<Iterator<Integer>> {
-//   Метод convert должен принимать объект итератор итератор и возвращать Итератор чисел.
-//   Iterator<Iterator<Integer> - ((4 2 0 4 6 4 9), (0 9 8 7 5), (1 3 5 6 7 0 9 8 4))
-//   Метод должен возвращать
-//   Iterator<Integer> - (4 2 0 4 6 4 9 0 9 8 7 5 1 3 5 6 7 0 9 8 4)
-//   Метод не должен копировать данные. Нужно реализовать итератор, который будет пробегать по вложенными итераторам без копирования данных.
+
+public class IteratorOfIterators {
 
     public Iterator<Integer> convert(Iterator<Iterator<Integer>> its) {
-        List<Integer> list = new ArrayList<>();
-        while (its.hasNext()) {
-            Iterator<Integer> it = its.next();
-//            System.out.println(Arrays.asList(it));
+        return new Iterator<>() {
+            Iterator<Integer> inner = its.next();
 
-            while (it.hasNext()) {
-                Integer i = it.next();
-                list.add(i);
-//                System.out.println(i);
+            @Override
+            public boolean hasNext() {
+                boolean hasNext = false;
+                if (inner != null) {
+                    hasNext = inner.hasNext();
+                    if ((!hasNext) && (its.hasNext())) {
+                        inner = its.next();
+                        hasNext = inner.hasNext();
+                    }
+                }
+                return hasNext;
             }
-        }
-        return list.iterator();
-    }
-    /**
-     *
-     * @return
-     */
-    @Override
-    public boolean hasNext() {
-        return false;
-    }
 
-    /**
-     *
-     * @return
-     */
-    @Override
-    public Iterator<Integer> next() {
-        return null;
+            @Override
+            public Integer next() {
+                Integer next;
+                if (inner.hasNext()) {
+                    next = inner.next();
+                } else if (its.hasNext()) {
+                        inner = its.next();
+                        next = inner.next();
+                    } else {
+                        throw new NoSuchElementException("No data found");
+                    }
+                return next;
+            }
+        };
     }
-
-
-//    public static void main(String[] args) {
-//        Iterator<Integer> it1 = Arrays.asList(1, 2, 3).iterator();
-//        Iterator<Integer> it2 = Arrays.asList(4, 5, 6).iterator();
-//        Iterator<Integer> it3 = Arrays.asList(7, 8, 9).iterator();
-//        Iterator<Iterator<Integer>> its = Arrays.asList(it1, it2, it3).iterator();
-//        IteratorOfIterators iteratorIterator = new IteratorOfIterators();
-//        Iterator<Integer> it = iteratorIterator.convert(its);
-//        while (it.hasNext()) {
-//            System.out.print(it.next() + " ");
-//        }
-//    }
 }
+
+
+

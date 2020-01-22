2b209b4f394b2d9f056f2ec3424827a33a8030c4
Andrey Varshavsky <nulnoil@outlook.com>
2020-01-22 18:08:47 +0300

    L2-001 3-5.3.2. Создать контейнер на базе связанного списка [#202653] Refactoring01

--- chapter_2_001/src/main/java/ru/job4j/list/SimpleListContainer.java
+++ chapter_2_001/src/main/java/ru/job4j/list/SimpleListContainer.java
@@ -1,41 +1,86 @@
 package ru.job4j.list;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
+/**
+ * Контейнер на базе связанного списка
+ * @author Andrey Varshavsky
+ * @version 1.0
+ * @since 21.01.2020
+ */
 public class SimpleListContainer<E> implements Iterable<E> {
+    private Node<E> first;
+    private int size;
 
-
-
-    public void add(E value) {
-
+    /**
+     * Метод вставляет в начало списка данные.
+     */
+    public void add(E data) {
+        Node<E> newNode = new Node<>(data);
+        newNode.next = this.first;
+        this.first = newNode;
+        this.size++;
     }
 
+    /**
+     * Метод получения элемента по индексу.
+     */
     public E get(int index) {
-
-        return null;
+        Node<E> result = this.first;
+        for (int i = 0; i < index; i++) {
+            result = result.next;
+        }
+        return result.data;
     }
 
-
-
-
-//    Итератор должен реализовывать fail-fast поведение, т.е. если с момента создания итератора коллекция подверглась структурному
-//        изменению, итератор должен кидать ConcurrentModificationException.
-//    Это достигается через введение счетчика изменений - modCount. Каждая операция, которая структурно модифицирует коллекцию
-//    должна инкрементировать этот счетчик. В свою очередь итератор запоминает значение этого счетчика на момент своего
-//    создания (expectedModCount), а затем на каждой итерации сравнивает сохраненное значение, с текущим значением поля modCount,
-//    если они отличаются, то генерируется исключение.
     @Override
     public Iterator<E> iterator() {
         return new Iterator<>() {
-            @Override
-            public boolean hasNext() {
-                return false;
+            final int iterableSize = size;
+            int iteratorPointer = 0;
+            Node<E> nextNode;
+
+            private void modifiedOrNot(int actualSize) {
+                if (iterableSize != actualSize) {
+                    throw new ConcurrentModificationException("Container has been changed!");
+                }
             }
 
             @Override
+            public boolean hasNext() {
+                modifiedOrNot(size);
+                return iteratorPointer < iterableSize;
+            }
+
+            @Override
+            @SuppressWarnings("unchecked")
             public E next() {
-                return null;
+                modifiedOrNot(size);
+                if (!hasNext()) {
+                    throw new NoSuchElementException("Element not found");
+                }
+                if (iteratorPointer == 0) {
+                    nextNode = first;
+                } else {
+                    nextNode = nextNode.next;
+                }
+                iteratorPointer++;
+                return (E) nextNode;
             }
         };
+    }
+
+    /**
+     * Класс предназначен для хранения данных.
+     */
+    private static class Node<E> {
+        E data;
+        Node<E> next;
+
+        Node(E data) {
+            this.data = data;
+        }
     }
 }
--- chapter_2_001/src/test/java/ru/job4j/list/SimpleListContainerTest.java
+++ chapter_2_001/src/test/java/ru/job4j/list/SimpleListContainerTest.java
@@ -1,4 +1,68 @@
 package ru.job4j.list;
 
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import ru.job4j.generics.SimpleArray;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 public class SimpleListContainerTest {
+    SimpleListContainer<Object> listContainer;
+
+    @Before
+    public void setUp() {
+        listContainer = new SimpleListContainer<>();
+        listContainer.add("First");
+        listContainer.add(125);
+        listContainer.add(true);
+        listContainer.add('&');
+        listContainer.add(1.25);
+    }
+
+    @Test
+    public void whenAddElementThanGetIt() {
+        Assert.assertThat(listContainer.get(0), is(1.25));
+        listContainer.add(1000);
+        Assert.assertThat(listContainer.get(0), is(1000));
+    }
+    @Test (expected = ConcurrentModificationException.class)
+    public void whenCreateIteratorThenModifyList() {
+        Iterator<Object> iterator = listContainer.iterator();
+        listContainer.add(1000);
+        iterator.next();
+    }
+
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenIterateUntilThrowsException() {
+        Iterator<Object> iterator = listContainer.iterator();
+        iterator.next();
+        iterator.next();
+        iterator.next();
+        iterator.next();
+        iterator.next();
+        iterator.next();
+    }
+    @Test
+    public void whenIterateUntilLast() {
+        Iterator<Object> iterator = listContainer.iterator();
+        Assert.assertTrue(iterator.hasNext());
+        iterator.next();
+        Assert.assertTrue(iterator.hasNext());
+        iterator.next();
+        Assert.assertTrue(iterator.hasNext());
+        iterator.next();
+        Assert.assertTrue(iterator.hasNext());
+        iterator.next();
+        Assert.assertTrue(iterator.hasNext());
+        iterator.next();
+        Assert.assertFalse(iterator.hasNext());
+    }
+
 }

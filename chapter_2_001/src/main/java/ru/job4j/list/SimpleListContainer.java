f624e32ad25379d0f8a833f2e0ecf5275b8e2c49
Andrey Varshavsky <nulnoil@outlook.com>
2020-01-28 11:11:27 +0300

    L2-001 3-5.3.2. Создать контейнер на базе связанного списка [#202653] Refactoring03

--- chapter_2_001/src/main/java/ru/job4j/list/SimpleListContainer.java
+++ chapter_2_001/src/main/java/ru/job4j/list/SimpleListContainer.java
@@ -10,39 +10,86 @@ import java.util.NoSuchElementException;
  * @version 1.0
  * @since 21.01.2020
  */
-public class SimpleListContainer<E> extends SimpleContainer<E> {
+public class SimpleListContainer<E> implements Iterable<E> {
+    private Node<E> head;
+    private Node<E> tail;
+    private int size = 0;
 
     /**
-     * Добавление данных в список
-     * @param data добавляемые данные
+     * Метод вставляет в начало списка данные.
      */
     public void add(E data) {
         Node<E> newNode = new Node<>(data);
         if (size == 0) {
-            this.first = newNode;
-            this.last = newNode;
+            this.head = newNode;
+            this.tail = newNode;
         } else {
-            newNode.previous = this.last;
-            this.last.next = newNode;
-            this.last = newNode;
+            newNode.previous = this.tail;
+            this.tail.next = newNode;
+            this.tail = newNode;
         }
         this.size++;
     }
 
     /**
-     * получение данных по индексу элемента списка
-     * @param index индекс элемента
-     * @return данные элемента
+     * Метод получения элемента по индексу.
      */
     public E get(int index) {
-        Node<E> result = this.first;
-        if (index < size) {
-            for (int i = 1; i <= index; i++) {
-                result = result.next;
-            }
-        } else {
+        if (index >= size) {
             throw new ArrayIndexOutOfBoundsException("Element does not exist yet.");
         }
+        Node<E> result = this.head;
+        for (int i = 1; i <= index; i++) {
+            result = result.next;
+        }
         return result.data;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<>() {
+            final int iterableSize = size;
+            int iteratorPointer = 0;
+            Node<E> nextNode = head;
+
+            private void modifiedOrNot(int actualSize) {
+                if (iterableSize != actualSize) {
+                    throw new ConcurrentModificationException("Container has been changed!");
+                }
+            }
+
+            @Override
+            public boolean hasNext() {
+                modifiedOrNot(size);
+                return iteratorPointer < iterableSize;
+            }
+
+            @Override
+            @SuppressWarnings("unchecked")
+            public E next() {
+                modifiedOrNot(size);
+                if (!hasNext()) {
+                    throw new NoSuchElementException("Element not found");
+                }
+                if (iteratorPointer > 0) {
+                    nextNode = nextNode.next;
+                }
+                iteratorPointer++;
+                return (E) nextNode;
+            }
+        };
+    }
+
+    /**
+     * Класс предназначен для хранения данных.
+     */
+    static class Node<E> {
+        E data;
+        Node<E> next;
+        Node<E> previous;
+
+        Node(E data) {
+            this.data = data;
+        }
     }
 }

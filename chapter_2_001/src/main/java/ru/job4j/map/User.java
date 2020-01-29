e51b92353aaff6f8723a2f6a774b3dcd19b7f372
Andrey Varshavsky <nulnoil@outlook.com>
2020-01-29 17:46:09 +0300

    L2-001 5-2. Не перекрывать equals hashCode[#202662] Refactoring01

---
+++ chapter_2_001/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,22 @@
+package ru.job4j.map;
+
+import java.util.*;
+
+/**
+ * Пользователь
+ * @author Andrey Varshavsky
+ * @version 1.0
+ * @since 27.01.2020
+ */
+
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+}
---
+++ chapter_2_001/src/test/java/ru/job4j/map/UserTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.map;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import java.util.*;
+
+public class UserTest {
+    User user1;
+    User user2;
+
+    @Before
+    public void beforeTest() {
+        user1 = new User("Dmitriy", 2, new GregorianCalendar(1990, Calendar.MARCH, 25));
+        user2 = new User("Dmitriy", 2, new GregorianCalendar(1990, Calendar.MARCH, 25));
+    }
+
+    @Test
+    public void whenAddTwoUsersShouldReturnSize2() {
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, 10001);
+        map.put(user2, 10002);
+        System.out.println(map);
+        Assert.assertThat(map.size(), is(2));
+    }
+}

acdb5256678986d3626c1daf09583a1577bb0e7c
Andrey Varshavsky <nulnoil@outlook.com>
2020-01-29 17:04:27 +0300

    L2-001 5-3. Переопределить только hashCode[#202660]

--- chapter_2_001/src/main/java/ru/job4j/map/User.java
+++ chapter_2_001/src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,6 @@
 package ru.job4j.map;
 
-import java.util.Calendar;
+import java.util.*;
 
 /**
  * Пользователь
@@ -18,5 +18,20 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+
+    public static void main(String[] args) {
+        User user1 = new User("Dmitriy", 2, new GregorianCalendar(1990, Calendar.MARCH, 25));
+        User user2 = new User("Dmitriy", 2, new GregorianCalendar(1990, Calendar.MARCH, 25));
+        Object object = new Object();
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, object);
+        map.put(user2, object);
+        System.out.println(map);
     }
 }
